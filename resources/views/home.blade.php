7e44e449447ea4fd512775b13ba9c3db319c8cd0
responsableproyecto <responsableproyecto@gmail.com>
2021-05-22 13:11:52 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+
+@section('cuerpo')
+    <div>
+        <h3>Productos Destacados</h3>
+        <div></div>
+    </div>
+    <div>
+        <h3>Categorias</h3>
+    </div>
+@endsection

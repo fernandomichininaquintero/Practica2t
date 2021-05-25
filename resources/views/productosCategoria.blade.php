f8deae82d820eab9f74e994b47ec4b6ad5c78786
responsableproyecto <responsableproyecto@gmail.com>
2021-05-25 18:08:55 +0200

    mejoras css

--- resources/views/productosCategoria.blade.php
+++ resources/views/productosCategoria.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('cuerpo')
-<div class="container">
+<div class="container p-3">
     <h3>Categoria: <?= $nombreCategoria->nombre?></h3>
     <?php
     $count = 0;
@@ -11,37 +11,39 @@
         }
         if ($count==0) {
     ?>
-            <div class="row">     
+            <div class="row"> 
     <?php
         }
     ?>
-            <div class="card mb-3 mr-2 p-2" style="width: 350px; height: 150px; text-align: center;">
-                <div class="row no-gutters">
-                    <div class="col-md-4">
-                        <img src="/../practica2t/assets/img/<?= $producto->imagen?>" alt="<?= $producto->nombre?>" height="150px" width="150px">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title"><?= $producto->nombre?></h5>
-                            <?php    
-                                if($producto->descuento>0){
-                                $descuento = ($producto->descuento * $producto->precio)/100;
-                                $precio = $producto->precio - $descuento;
-                            ?>
-                                    <p class="card-text"><small class="text-muted"><?=$producto->precio?>€</small></p>
-                                    <p class="card-text"><small style="color: red;"><?= $producto->descuento?>%</small> <?= $precio?>€</p>
-                            <?php
-                                }else{
-                            ?>
-                                <p class="card-text"><?=$producto->precio?>€</p>
-                            <?php
-                                }
-                            ?>
-                            
+                <div class="col-lg-4 col-sm-6">
+                    <div class="card mb-3 mr-2">
+                        <div class="row no-gutters">
+                            <div class="col-5 col-md-5">
+                                <img src="/../practica2t/assets/img/<?= $producto->imagen?>" class="card-img" alt="<?= $producto->nombre?>" height="150px">
+                            </div>
+                            <div class="col-7 col-md-7">
+                                <div class="card-body">
+                                    <h5 class="card-title"><?= $producto->nombre?></h5>
+                                    <?php    
+                                        if($producto->descuento>0){
+                                        $descuento = ($producto->descuento * $producto->precio)/100;
+                                        $precio = $producto->precio - $descuento;
+                                    ?>
+                                            <p class="card-text"><small class="text-muted"><?=$producto->precio?>€</small></p>
+                                            <p class="card-text"><small style="color: red;"><?= $producto->descuento?>%</small> <?= $precio?>€</p>
+                                    <?php
+                                        }else{
+                                    ?>
+                                        <p class="card-text"><?=$producto->precio?>€</p>
+                                    <?php
+                                        }
+                                    ?>
+                                    
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    </div> 
                 </div>
-            </div> 
     <?php
     $count++;
         if ($count==3) {
